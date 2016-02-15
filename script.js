9c2a1a6eaa3a21e548fc8285394cf474b53be20e
stefanovtamara <stefanov.tamara@gmail.com>
2016-02-15 17:35:48 -0500

    working to end game after 15pt

--- script.js
+++ script.js
@@ -1,16 +1,71 @@
 $(document).ready(function(){
   var turn = 0;
+
   $('td').click(function(){
-    var self = $(this);
+    var self  = $(this);
 
     if (turn % 2){
-      self.html('O')
+      self.html('O').addClass('o');
     }else{
-      self.html('X')
+      self.html('X').addClass('x');
     }
 
     self.off('click');
+    checkWinner();
+
     turn ++;
 
   });
+  function checkWinner(){
+    var cellX = $('.x')
+    var cellO = $('.o')
+
+    for (var i = 0; i < cellX.length; i++){
+      for (var y = i+1; y < cellX.length; y++){
+        for (var z = y+1; z < cellX.length; z++){
+          if(parseInt(cellX[i].id) + parseInt(cellX[y].id) + parseInt(cellX[z].id) === 15){
+            xWins();
+            playAgain();
+
+          }
+        }
+      }
+    }
+    for (var i = 0; i < cellO.length; i++){
+      for (var y = i+1; y < cellO.length; y++){
+        for (var z = y+1; z < cellO.length; z++){
+          if(parseInt(cellO[i].id) + parseInt(cellO[y].id) + parseInt(cellO[z].id) === 15){
+            alert('O wins!')
+            playAgain();
+          }
+        }
+      }
+    }
+    function xWins(){
+      count = 0
+      winX = window.confirm('X Won?')
+      if (winX == true){
+        count++
+        $('#x').html(count)
+      }
+    }
+    // function playAgain(){
+    //   again = window.confirm('Play Again?')
+    //   if (again == true){
+    //     location.reload();
+    function playAgain(){
+      again = window.confirm('play again?')
+      if(again == true){
+
+        $('td').empty('.o')
+        $('td').empty('.x')
+      }
+
+    }
+
+  }
+
+
+
+
 });
